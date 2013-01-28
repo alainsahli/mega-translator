6a770eacea53cf686dddfb15f66d648149a85625
Etienne Studer <etienne@studer.nu>
2013-01-28 23:34:54 +0100

    Fix typo

--- src/test/scala/TranslatorTest.scala
+++ src/test/scala/TranslatorTest.scala
@@ -3,7 +3,7 @@ import org.scalatest.FunSpec
 class TranslatorTest extends FunSpec {
 
   describe("When adding two integers") {
-    it("the sum should be ccalculated") {
+    it("the sum should be calculated") {
       assert(Translator.sum(2, 3) === 5)
     }
   }
