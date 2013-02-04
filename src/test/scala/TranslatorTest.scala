86adc346b1099b346c24f59eb3693db7e5a1dfd0
Etienne Studer <etienne@studer.nu>
2013-02-04 18:04:49 +0100

    Add JUnit test

--- src/test/scala/TranslatorTest.scala
+++ src/test/scala/TranslatorTest.scala
@@ -1,11 +1,17 @@
-import org.scalatest.FunSpec
+import org.junit.Test
+import org.scalatest.junit.AssertionsForJUnit
 
-class TranslatorTest extends FunSpec {
+class TranslatorTest extends AssertionsForJUnit {
 
-  describe("When adding two integers") {
-    it("the sum should be calculated") {
-      assert(Translator.sum(2, 3) === 5)
-    }
+  //  describe("When adding two integers") {
+  //    it("the sum should be calculated") {
+  //      assert(Translator.sum(2, 3) === 5)
+  //    }
+  //  }
+
+  @Test
+  def sum() {
+    assert(Translator.sum(2, 3) === 5)
   }
 
 }
