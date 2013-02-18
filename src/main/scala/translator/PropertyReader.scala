3c33b0abdb228ac495a34d9f627e2cc607d71eae
Etienne Studer <etienne@studer.nu>
2013-02-18 17:51:30 +0100

    Use extractor

--- src/main/scala/translator/PropertyReader.scala
+++ src/main/scala/translator/PropertyReader.scala
@@ -21,9 +21,9 @@ object PropertyReader {
       if (matches.isEmpty) result
       else {
         val valuesByPrefix = matches.groupBy(_ take minLength)
-        val valuesOfMostCommonPrefix = valuesByPrefix.maxBy(_._2.size)
-        if (valuesOfMostCommonPrefix._2.size < 2) result
-        else inferPrefix(valuesByPrefix(valuesOfMostCommonPrefix._1), minLength + 1, Some(valuesOfMostCommonPrefix._1))
+        val (prefix, elements) = valuesByPrefix.maxBy(_._2.size)
+        if (elements.size < 2) result
+        else inferPrefix(valuesByPrefix(prefix), minLength + 1, Some(prefix))
       }
     }
 
